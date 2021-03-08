45f46e5ff964c1696b4f95bb718de59bd4bb60cd
KANTY <KANTY@DESKTOP-5KBC3FN>
2021-03-08 14:23:11 +0100

    second commit

---
+++ test_python.py
@@ -0,0 +1,10 @@
+#Les test en python se fait à l aide de assert et ca met assertion error aux cas ou la condition n est pas respecter
+def positif(a,b):
+    x = a + b
+    assert x >= 0
+def negatif(a,b):
+    x = a + b
+    assert x < 0
+
+positif(2,4)
+positif(2,-4)
