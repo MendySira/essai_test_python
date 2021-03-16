69cb40f22b02f80094319061b9e121fbd5554362
MendySira <45494336+MendySira@users.noreply.github.com>
2021-03-16 17:13:56 +0100

    Update test_python.py

--- test_python.py
+++ test_python.py
@@ -1,7 +1,14 @@
-#import tp_casino.py
 from tp_casino import Joueur
+from tp_casino import Casino
+from tp_casino import getArgentMise
+from tp_casino import getNumeroMise
 
 j1 = Joueur("The Mike", 2000)
+c1 = Casino("Casa Blanca", j1)
 
 def test_create_user():
     assert j1 != None, "le joueur n'a pas été crée"
+    
+
+def test_argent_positif():
+    assert j1.argent >= 0, "l'argent de l'utilisateur est  négatif "
