7b1fd6c3e4d7a633efae09f58e1d362a56598c71
KOKO Ambinintsoa Kantisambatra <45491492+kokokanti98@users.noreply.github.com>
2021-03-16 11:58:18 +0100

    Update test_python.py

--- test_python.py
+++ test_python.py
@@ -1,9 +1,9 @@
 #Les test en python se fait à l aide de assert et ca met assertion error aux cas ou la condition n est pas respecter
-def positif(a,b):
+def somme(a,b):
     x = a + b
+    return x;
+def test_somme():
+    a = 2
+    b = 4
+    x = somme(a,b)
     assert x >= 0
-def negatif(a,b):
-    x = a + b
-    assert x < 0
-
-positif(2,4)
